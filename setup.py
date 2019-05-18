bed85dde97abfaa95773d0ae3cc5c8d31208d552
Ravique <dofin-agas@ya.ru>
2019-05-18 18:02:12 +0300

    dirty, but works. maybe. tests needed + setup.py + bare README.md :)

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="otus-homework1",
+    version="dev",
+    author="Andrei Etmanov",
+    author_email="andres@space-coding.ru",
+    description="Script for static code analysis. Search for different parts of speech in function names in .py files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ravique/otus-homework1",
+    py_modules={'report_writers', 'words_parser'},
+    packages=setuptools.find_packages(),
+    license="MIT",
+)
